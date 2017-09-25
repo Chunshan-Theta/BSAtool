d16c7eec7a2e0c0d7660c61ced2b808850718889
gavin <f106524018@g.ncu.edu.tw>
2017-09-25 14:03:15 +0800

    add remo

--- Csv2BS.py
+++ Csv2BS.py
@@ -1,5 +1,19 @@
 #coding:utf-8
 #Csv file of Source Data convert to Behavior Sequential 
+'''
+using :
+
+Set var:
+    CsvDir ='DataFormWuret.csv'     #Data from and name 
+    listtitle = 'group_argu_code'   #Row name 
+    TypeNum = 6                     #Number of motion type
+    SelectedGroup = '21'            #Name of Selected Group
+Read file:
+    SourceData = ReadFile(CsvDir)
+
+Compute Motion array:
+    ComputeMotionIndex(SourceData,TypeNum,'21')
+'''
 import csv
 import numpy
 
@@ -16,7 +30,9 @@ def ComputeMotionIndex(listmotion,TypeNum,wantSet='-1'):
         MotionSet[index+1][0]=index+1
 
     for index in range(len(listmotion)-1):
-        if listmotion[index][0] == listmotion[index+1][0] and (wantSet == listmotion[index][0] or wantSet == '-1'): # if not same group , pass
+        if listmotion[index][0] == listmotion[index+1][0] and (wantSet == listmotion[index][0] or wantSet == '-1'):
+            # if not same group, pass
+            # if didn't select group, pass
             FirstMotion = listmotion[index][1]
             SecondMotion = listmotion[index+1][1]
             MotionSet[FirstMotion][SecondMotion] += 1
@@ -36,14 +52,8 @@ def ReadFile(FileDir):
 
 
 
-
-
-
-
-
-
 CsvDir ='DataFormWuret.csv' 
 listtitle = 'group_argu_code'
 TypeNum = 6
-
-ComputeMotionIndex(ReadFile(CsvDir),TypeNum,'21')
+SelectedGroup = '21'
+ComputeMotionIndex(ReadFile(CsvDir),TypeNum,SelectedGroup)
