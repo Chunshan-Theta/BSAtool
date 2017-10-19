4b930625865e20618abbf4045d9439534484e1e3
gavin <f106524018@g.ncu.edu.tw>
2017-10-19 12:29:56 +0800

    update

--- catch.py
+++ catch.py
@@ -10,7 +10,8 @@ sys.setdefaultencoding('utf-8')     # 3
 import MySQLdb
 
 # 連接 MySQL 資料庫
-db = MySQLdb.connect(host="xxx.xxxx.xxxx.xxxx",user="root", passwd="root_password", db="db_liat_name",charset='utf8')
+#db = MySQLdb.connect(host="xxx.xxxx.xxxx.xxxx",user="root", passwd="root_password", db="db_liat_name",charset='utf8')
+db = MySQLdb.connect(host="140.118.37.196",user="root", passwd="ytwu57874", db="ets_search",charset='utf8')
 cursor = db.cursor()
 
 
@@ -129,26 +130,43 @@ ShowDBList(note)
 note = DBfilter_sorting(note,[3,2,1,0])
 ShowDBList(note)
 '''
-#ShowDBList(note)                                #show data of SQL list
-note = DeleteRow(note,5,'')                     #delete rows that don't have quit time
-note = DeleteRow(note,2,'')                     #delete rows that don't have content of note
-note = DBfilter_keep_column(note,[2,3,5])       #keep some column and drop other columns
-note = DBfilter_sorting(note,[1,0,2])           #Reset sort of list
-note = Addcolumn(['筆記'],note)                 #Add new column to list
-#ShowDBList(note)
-
-
-ShowDBList(search_web)                          #show data of SQL list
-'''
-note = DeleteRow(note,5,'')                     #delete rows that don't have quit time
-note = DeleteRow(note,2,'')                     #delete rows that don't have content of note
-note = DBfilter_keep_column(note,[2,3,5])       #keep some column and drop other columns
-note = DBfilter_sorting(note,[1,0,2])           #Reset sort of list
-note = Addcolumn(['筆記'],note)                 #Add new column to list
+#ShowDBList(note)                                           #show data of SQL list
+note = DeleteRow(note,5,'')                                 #delete rows that don't have quit time
+note = DeleteRow(note,2,'')                                 #delete rows that don't have content of note
+note = DeleteRow(note,3,'')                                 #delete rows that don't have id of student 
+note = DBfilter_keep_column(note,[2,3,5])                   #keep some column and drop other columns
+note = DBfilter_sorting(note,[1,0,2])                       #Reset sort of list
+note = Addcolumn(['筆記'],note)                             #Add new column to list
 ShowDBList(note)
-'''
 
-#arr = [['type','stu_id','content','time']]
+
+#ShowDBList(search_web)                                     #show data of SQL list
+search_web = DeleteRow(search_web,1,'')                     #delete rows that don't have id of student 
+search_web = DeleteRow(search_web,2,'')                     #delete rows that don't have title of web
+search_web = DeleteRow(search_web,4,'')                     #delete rows that don't have quit time
+search_web = DBfilter_keep_column(search_web,[1,2,4])       #keep some column and drop other columns
+search_web = Addcolumn(['瀏覽網頁'],search_web)             #Add new column to list
+ShowDBList(search_web)
+
+#ShowDBList(search)                                         #show data of SQL list
+search = DeleteRow(search,11,'')                            #delete rows that don't have id of student 
+search = DeleteRow(search,7,'')                             #delete rows that don't have title of web
+search = DeleteRow(search,8,'')                             #delete rows that don't have quit time
+search = DBfilter_keep_column(search,[7,8,11])              #keep some column and drop other columns
+search = DBfilter_sorting(search,[2,0,1])                   #Reset sort of list
+search = Addcolumn(['搜索關鍵字'],search)                    #Add new column to list
+ShowDBList(search)                                          
+
+
+#ShowDBList(chat_record)                                    #show data of SQL list
+chat_record = ReplaceContent(chat_record,'辯論者','s10')    #Correct dirty content of id
+chat_record = DeleteRow(chat_record,1,'')                   #delete rows that don't have id of student 
+chat_record = DeleteRow(chat_record,2,'')                   #delete rows that don't have content of chat
+chat_record = DeleteRow(chat_record,4,'')                   #delete rows that don't have quit time
+chat_record = DBfilter_keep_column(chat_record,[1,2,4])     #keep some column and drop other columns
+chat_record = Addcolumn(['論證'],chat_record)               #Add new column to list
+ShowDBList(chat_record)                                     #show data of SQL list
+
 
 
 # 關閉連線
