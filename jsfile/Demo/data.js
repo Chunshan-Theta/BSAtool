66427deaf7b0c079b023e01aa1a30fd0ed75d513
Wang <gavin19950511@gmail.com>
2019-04-15 10:53:40 +0800

    Create data.js

---
+++ jsfile/Demo/data.js
@@ -0,0 +1,138 @@
+var actionLog =[
+  {
+    "bid": 353,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【提出】我認為",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T06:44:34.000Z"
+  },
+  {
+    "bid": 366,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【推理】123",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:02:45.000Z"
+  },
+  {
+    "bid": 367,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】1",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:11:12.000Z"
+  },
+  {
+    "bid": 368,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】你說的是不是,@$&,%@,山豬 : 【疑問】你為什麼會認為,@$&,%@,355",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:14:20.000Z"
+  },
+  {
+    "bid": 369,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】你說的是不是",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:15:31.000Z"
+  },
+  {
+    "bid": 370,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】你能不能進一步仔細說明,@$&,%@,鄭宇傑 : 【疑問】你說的是不是,@$&,%@,369",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:23:15.000Z"
+  },
+  {
+    "bid": 371,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【推理】111,@$&,%@,鄭宇傑 : 【疑問】你能不能進一步仔細說明,@$&,%@,370",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:23:36.000Z"
+  },
+  {
+    "bid": 372,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】你為什麼會認為",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:34:28.000Z"
+  },
+  {
+    "bid": 373,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【挑戰】我不同意你的論點，因為",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:35:02.000Z"
+  },
+  {
+    "bid": 374,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【推理】我所提出的反論點是",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:36:13.000Z"
+  },
+  {
+    "bid": 375,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】1111111111111111111111111111111111",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:37:30.000Z"
+  },
+  {
+    "bid": 376,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】222222222222222222222222222222222",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:39:52.000Z"
+  },
+  {
+    "bid": 377,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】333333333333333333333333333333",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:40:23.000Z"
+  },
+  {
+    "bid": 384,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【提出】讓我提出證據",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T07:47:05.000Z"
+  },
+  {
+    "bid": 391,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】6666666,@$&,%@,山豬 : 【提出】55555,@$&,%@,390",
+    "name": "鄭宇傑",
+    "time": "2019-01-31T08:07:40.000Z"
+  },
+  {
+    "bid": 423,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】你能不能進一步仔細說明 yey",
+    "name": "鄭宇傑",
+    "time": "2019-02-20T04:35:36.000Z"
+  },
+  {
+    "bid": 424,
+    "tid": "2-garret456963@gmail.com",
+    "Type": "message",
+    "content": "【疑問】你能不能進一步仔細說明 try  to build on\n,@$&,%@,山豬 : 【疑問】vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv,@$&,%@,401",
+    "name": "鄭宇傑",
+    "time": "2019-02-20T04:38:34.000Z"
+  }
+]
